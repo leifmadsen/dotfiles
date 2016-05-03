f4496fb269874db9067834c4d0e6e5275d0f4417
Leif Madsen <lmadsen@redhat.com>
2016-05-03 15:17:22 -0400

    Latest round of changes
    * adjust bashrc again for lolcommits video device
    * update powerline config with correct json formatting
    * add xfce4-power-manager to i3 startup
    * remove old airline and ansible-vim vim bundles
    * update gmail.sh script to use a better method to get unread mails
    * add FormatJSON function to vim

--- bin/gmail.sh
+++ bin/gmail.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Script para ver la cantidad de correos de Gmail.
@@ -6,12 +5,18 @@
 # -c -> Guarda resultado en ~/.gmail (para conky)
 # 12 ene 2015
 
+# Available system labels
+# https://developers.google.com/gmail/android/com/google/android/gm/contentprovider/GmailContract.Labels.LabelCanonicalNames#constants
+#
+# Originally found at https://stackoverflow.com/questions/24959370/list-of-gmail-atom-available-labels/25515535#25515535
+
 # Ruta a los password
 . ~/.private/accounts
 
-MAIL=`curl -u $MAIL_USER:"$MAIL_PASS" --silent https://mail.google.com/mail/feed/atom`
+MAIL=`curl -u $MAIL_USER:"$MAIL_PASS" --silent https://mail.google.com/mail/feed/atom/%5Ei`
 CON=`echo $MAIL | grep -c "<fullcount>"`
-NUM=`echo $MAIL | grep -o "<entry>" | wc -l`
+NUM=`echo $MAIL | xml_grep 'fullcount' --text_only`
+#NUM=`echo $MAIL | grep -o "<entry>" | wc -l`
 RES="err"
 
 if [ $CON -ne 1 ]; then
