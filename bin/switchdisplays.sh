be5ced5558664bdbd17d8c26ddd2b0695e5624fe
Leif Madsen <lmadsen@redhat.com>
2016-07-12 14:50:25 -0400

    Need --crtc for switchdisplays.sh now...

--- bin/switchdisplays.sh
+++ bin/switchdisplays.sh
@@ -5,8 +5,8 @@ loc=$1
 if [ "$loc" = "desk" ] ; then
     xrandr --output eDP1 --off
 	xrandr --output DP2-2 --auto --right-of DP2-3
-	xrandr --output DP2-1 --auto --left-of DP2-3
-	xrandr --output DP2-3 --auto --left-of DP2-2
+	xrandr --output DP2-1 --auto --left-of DP2-3 --crtc 2
+	xrandr --output DP2-3 --auto --left-of DP2-2 --crtc 1
 elif [ "$loc" = "laptop" ] ; then
 	xrandr --output DP2-1 --off
 	xrandr --output DP2-2 --off
